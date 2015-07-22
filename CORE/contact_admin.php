1b0be61fdf147b3b04ba040fc3f570ee89c6a669
Yordan Alvarez <yalva054@fiu.edu>
2015-07-22 19:13:50 -0400

    added contact us page, profile page, contact admin

--- CORE/contact_admin.php
+++ CORE/contact_admin.php
@@ -1,57 +1,25 @@
 <?php   
-        //user contact admin in index.php
+        //contact us in home page
 
 	//import general functions
 	require "functions.php";
-        require_once '/Applications/MAMP/htdocs/GenomePro/phpmailer/vendor/autoload.php';  // importing the phpmailer functionality
 	
-	//check input variables exist
-	if(isEmpty(@$_POST['from']) || isEmpty(@$_POST['subject']) || isEmpty(@$_POST['msg'])) die ("missing data.");	
-	
+	//check input variables exist	
+	if(isEmpty(@$_POST['name'])) throw new GeneralException('Please enter your name.', 010);
+        else if (isEmpty(@$_POST['from'])) throw new GeneralException('Please enter your email.', 011);
+        else if (isEmpty(@$_POST['subject'])) throw new GeneralException('Please enter a subject.', 012);
+        else if(isEmpty(@$_POST['msg'])) throw new GeneralException('Please enter a message.', 013);
+            
 	//read variables from register user form
+        $name = $_POST['name'];
 	$email = $_POST['from'];
 	$subject = $_POST['subject'];
 	$msg = $_POST['msg'];
-	
-
-	//send email to user for him to know that needs to activate its account
-	//mail($adminEmail,"Email Contact from ".$from.", subject: ".$subject, $msg);
-	
+        
+        //send an email to the admin from the user, when upload to server needs to be changed
+        emailAdmin($email, $name, $subject, $msg);
+      
 	//return sent to client	
-	//returnValue("sent.");
-        
-       
-        $from = 'yordan.alvarez30@gmail.com';
-        $senderName = 'Yordan Alvarez';
-        $to         = $email;       // destination
-        $sub        = $subject;     // subject
-        $message    = $msg;         // message
-        
-
-        // using gmail accounts for testing, we can change it once is on the server 
-        $m = new PHPMailer;       // new php mailer object
-
-        $m->isSMTP();             // telling phpmailer we want to use the smpt option
-        $m->SMTPAuth = true;      // testing properties, for debuging
-
-        $m->Host = 'smtp.gmail.com';                   // the smtp for gmail
-        $m->Username = $from;                          // own user name 
-        $m->Password = 'yordanalvarez30';              // password
-        $m->SMTPSecure = 'ssl';                        // secure type
-        $m->Port = 465;                                // port used
-
-        $m->From = $from;                              // email sending from, will be genomepro.cis.fiu.edu
-        $m->FromName = $senderName;                    // here will be GnomePro Team
-        $m->addReplyTo($from, 'Reply address');        // method to reply to, here will be genomepro.cis.fiu.edu
-        $m->addAddress($to, $senderName);              // send this to users email (To)
-
-        $m->Subject = $sub;                            // email subject
-        $m->Body = $message;                           // message of the email 
-                                       
-
-        if($m->send()) // email was successfully ACCEPTED for delivery
-        {
-            returnValue("sent.");
-        }
-               
+	returnValue("sent.");
+                            
 ?>
